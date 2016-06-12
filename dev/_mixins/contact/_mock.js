821c50f81d13e2ae8c55eea88bed32d990b35ba9
Albert-0 MS <Albert-0 MS>
2016-06-11 23:21:27 -0500

    (fix(contact)):terminando el form contacto

--- dev/_mixins/contact/_mock.js
+++ dev/_mixins/contact/_mock.js
@@ -1,5 +1,15 @@
 module.exports = {
 	name: 'Nombre',
-	email: '',
-	save: 'Send'
+	email: 'Correo Electrónico',
+	telefono:'Telefono',
+	servicio:'Servicio Solicitado',
+	ciudad:'Seleccione la Ciudad',
+	dudas:'Indicanos tus dudas',
+	save: 'Solicitar Información',
+	servicio1:"ORTODONCIA",
+	servicio2:"LIMPIEZA",
+	ciudad1:"CIUDAD DE MEXICO",
+	ciudad2:"IGUALA DE LA INDEPENDENCIA",
+	dudas:"Indicanos tus dudas"
+
 }
